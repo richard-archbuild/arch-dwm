bc148fe0e57a87e4b2e22121d4a49cf4dace8af6
richard-archbuild <73938516+richard-archbuild@users.noreply.github.com>
2020-11-22 00:11:48 +0200

    Update autostart.sh

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-picom --experimental-backends -b
-wal -i /home/michael/Pictures/Wallpapers/Rotes_Boot.jpg
+#wal -i /home/michael/Pictures/Wallpapers/Rotes_Boot.jpg
 while true 
 do
 	battery="$(acpi | sed -n -e 's/Battery 0: //;s/%.*/%/;s/,//g' -e '1p')"
