58f46af6a937b0d63b8969b729b46d8bfd7aaebd
richard-archbuild <73938516+richard-archbuild@users.noreply.github.com>
2020-11-23 20:58:17 +0200

    Update config.h

--- st/config.h
+++ st/config.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
-#include "/home/michael/.cache/wal/colors-wal-st.h"
+#include "HOME/.cache/wal/colors-wal-st.h"
 
 
 
