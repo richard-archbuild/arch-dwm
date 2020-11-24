44f2a00193a227921a34d2ee6019cc557079145a
richard-archbuild <73938516+richard-archbuild@users.noreply.github.com>
2020-11-25 00:22:18 +0200

    Update after-arch.sh

--- after-arch.sh
+++ after-arch.sh
@@ -3,5 +3,8 @@ echo "username";read user
 useradd -m -g wheel "$user"
 passwd "$user"
 
+sudo pacman -S wget
+curl "https://raw.githubusercontent.com/richard-archbuild/arch-dwm/main/firstrun.sh" > /home/$user/firstrun.sh 
+
 sed -i 's/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers 
 echo "Now Log In As The New User"
