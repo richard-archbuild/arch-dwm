a6129d583dcc268f351b9442be6f1e568d6eb018
richard-archbuild <73938516+richard-archbuild@users.noreply.github.com>
2020-11-24 15:15:24 +0200

    Update WallpaperChange.sh

--- Scripts/WallpaperChange.sh
+++ Scripts/WallpaperChange.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 wall="$1"
 wal -i "$wall"
-sed  -i "s/.*Wallpaper.*/wal -i \/home\/michael\/Pictures\/Wallpapers\/$wall/" $HOME/.dwm/autostart.sh
-cd "$HOME/dwm"
+cd "$HOME/.dwm"
+sudo make clean install
+cd $HOME/.st
 sudo make clean install
 exit
