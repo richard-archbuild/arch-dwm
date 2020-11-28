8f6c660d460d8c4d2b7c88967b8e7144a8e03605
richard-archbuild <73938516+richard-archbuild@users.noreply.github.com>
2020-11-28 17:18:32 +0200

    Delete RandomWord.sh

--- Scripts/RandomWord.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-num=$(wc -l < $HOME/Documents/School/TVC/German/Words.csv)
-random=$(shuf -i 1-$num -n 1)
-word="$(sed -n -e 's/,.*//'  -e "$random"p $HOME/Documents/School/TVC/German/Words.csv)"
-translate="$(echo $word | dmenu -h 24 -p "Random Word")"
-[ "$translate" == "no" ] && grep "$word" $HOME/Documents/School/TVC/German/Words.csv | cut -d, -f2 | dmenu
-exit
