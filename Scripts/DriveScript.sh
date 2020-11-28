a47dddd54131f6d6e19825fda2b64f79d33ff9fb
richard-archbuild <73938516+richard-archbuild@users.noreply.github.com>
2020-11-28 17:24:08 +0200

    Update DriveScript.sh

--- Scripts/DriveScript.sh
+++ Scripts/DriveScript.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 drive=$(lsblk -l | grep "part \'" | awk '{print $1}' | dmenu -l 5 -h 24) 
-passwd=$(echo "" | dmenu -p "Root Password" -h 24)
-echo "$passwd" | sudo -S mkdir /mnt/"$drive" 
-echo "$passwd" | sudo -S mount /dev/"$drive" /mnt/"$drive"/ && 
+sudo mkdir /mnt/"$drive" 
+sudo mount /dev/"$drive" /mnt/"$drive"/ && 
 	{
 	echo "" | dmenu -p "Mounted: $drive" || echo "" | dmenu -p "Could Not Mount: $drive" 
 	}
