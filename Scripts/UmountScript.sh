bc7870e3a639c5af9fef25e5962ffb16df997ce9
richard-archbuild <73938516+richard-archbuild@users.noreply.github.com>
2020-11-28 17:29:24 +0200

    Update UmountScript.sh

--- Scripts/UmountScript.sh
+++ Scripts/UmountScript.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-. "$HOME/.cache/wal/colors.sh"
-drive=$(lsblk -l | grep "/." | awk '{print $7}' | dmenu -h 24 -l 5 )
-passwd=$(echo "" | dmenu -h 24 -p "Root Password for unmounting $drive" ) 
+drive=$( lsblk -lno mountpoint | grep -v "^$\|\/$\|\/boot$"  | dmenu -h 24 -l 5 )
 
-echo "$passwd" | sudo -S umount "$drive" && 
+sudo umount "$drive" && 
 	{
 	echo "" | dmenu -p "Unmounted: $drive" || echo "" | dmenu -p "Could Not Unmount: $drive"
 	}
 
-echo "$passwd" | sudo -S rm -r "$drive"
+sudo rm -r "$drive"
 exit
