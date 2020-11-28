36a5af1e141175d20f3cdb6ba5022d5001ae1717
richard-archbuild <73938516+richard-archbuild@users.noreply.github.com>
2020-11-28 17:18:04 +0200

    Delete WordScript.sh

--- Scripts/WordScript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-Word=$(cat $HOME/Documents/School/TVC/German/Words.csv | dmenu -i -l 30)
-[ -z "$Word" ] && exit || echo "$Word" >> $HOME/Documents/School/TVC/German/Words.csv
-cd $HOME/Documents/School/TVC/German/
-sort Words.csv >> Word.csv
-rm Words.csv; mv Word.csv Words.csv
